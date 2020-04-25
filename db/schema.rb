5dccb9a904a3525b4c690d76dd40c87e80ac417e
oohashi <hongshudaqiao@gmail.com>
2020-04-25 14:17:32 +0000

    6章2-2の2個まで終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420143555) do
+ActiveRecord::Schema.define(version: 20200424144830) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
