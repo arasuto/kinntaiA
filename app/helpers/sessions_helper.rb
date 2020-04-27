4db8961c3f729fa0c43ef8dcf7ee774c7bb18e8e
oohashi <hongshudaqiao@gmail.com>
2020-04-27 12:34:46 +0000

    Finish login-function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,13 @@ module SessionsHelper
  end
  
  
+ # セッションと@current_userを破棄します
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  
  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
   def current_user
     if session[:user_id]
