39b255b470df5c05eb62e69643b7ea99324d410d
oohashi <hongshudaqiao@gmail.com>
2020-05-01 14:30:53 +0000

    8の3まで終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:show, :edit, :update]
-before_action :correct_user, only: [:edit, :update]
-before_action :correct_user, only: [:edit, :update]
-
-
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  
+  def index
+    @users = User.all
+  end
+  
+  
   def show
   end
 
@@ -14,19 +19,17 @@ before_action :correct_user, only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user 
-    flash[:success] = '新規作成に成功しました。'
-    redirect_to @user
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
     else
       render :new
     end
   end
-  
-  
+
   def edit
   end
-  
-  
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
@@ -38,22 +41,21 @@ before_action :correct_user, only: [:edit, :update]
 
   private
 
-
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
     # beforeフィルター
-    
-    
+
     # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
-    
-    
+
     # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
+      store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
@@ -61,6 +63,6 @@ before_action :correct_user, only: [:edit, :update]
 
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
