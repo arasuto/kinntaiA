5ef6794e3fbe3d209c35623db29212d4710da910
EC2 Default User <ec2-user@ip-172-31-18-81.ap-northeast-1.compute.internal>
2021-01-01 09:36:18 +0000

    残業申請1終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, ]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :edit_overwork_requsest]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit]
   before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
